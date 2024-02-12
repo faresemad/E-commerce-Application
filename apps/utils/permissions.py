c9506bacb2fd44d2bad65eee308913114fa50cc5
Fares Emad <faresemadx@gmail.com>
2024-02-12 19:10:15 +0200

    build project files and update readme file

---
+++ apps/utils/permissions.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.models import AnonymousUser
+from rest_framework import permissions
+from rest_framework.permissions import IsAuthenticated
+
+# TODO: Create permissions for the following:
+# - IsOwnerOrReadOnly
+# - IsOwner
+
+
+class IsOwnerOrReadOnly(IsAuthenticated):
+    """
+    Custom permission to only allow owners of an object to edit it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if isinstance(request.user, AnonymousUser):
+            return False
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        # Write permissions are only allowed to the owner of the snippet.
+        return obj.user == request.user
+
+
+class IsOwner(IsAuthenticated):
+    """
+    Custom permission to only allow owners of an object to edit it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        if isinstance(request.user, AnonymousUser):
+            return False
+        return obj.user == request.user
