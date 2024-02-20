8bc7f0bf9cbea1f177b1b410905ace958dd6adb8
Fares Emad <faresemadx@gmail.com>
2024-02-20 17:28:53 +0200

    refactor: :recycle: Refactor cart and cart item serializers and views

--- apps/carts/api/views.py
+++ apps/carts/api/views.py
@@ -1,8 +1,12 @@
-from rest_framework import mixins, status, viewsets
+from rest_framework import mixins, viewsets
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
 
-from apps.carts.api.serializers import CartItemSerializer, CartSerializer
+from apps.carts.api.serializers import (
+    CartCreateUpdateSerializer,
+    CartItemCreateUpdateSerializer,
+    CartItemListRetrieveSerializer,
+    CartListRetrieveSerializer,
+)
 from apps.carts.models import Cart, CartItem
 
 
@@ -13,37 +17,16 @@ class CartViewSet(
     mixins.UpdateModelMixin,
     viewsets.GenericViewSet,
 ):
-    serializer_class = CartSerializer
+    serializer_class = CartCreateUpdateSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         return Cart.objects.filter(user=self.request.user)
 
-    def perform_create(self, serializer):
-        cart = serializer.save()
-        cart.user = self.request.user
-        cart.save()
-
-    def retrieve(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = CartSerializer(instance)
-        return Response(serializer.data)
-
-    def update(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = CartSerializer(instance, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-
-    def destroy(self, request, *args, **kwargs):
-        instance = self.get_object()
-        self.perform_destroy(instance)
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-    def perform_destroy(self, instance):
-        instance.products.clear()
-        instance.save()
+    def get_serializer_class(self):
+        if self.action in ["list", "retrieve"]:
+            return CartListRetrieveSerializer
+        return CartCreateUpdateSerializer
 
 
 class CartItemViewSet(
@@ -53,34 +36,13 @@ class CartItemViewSet(
     mixins.UpdateModelMixin,
     viewsets.GenericViewSet,
 ):
-    serializer_class = CartItemSerializer
+    serializer_class = CartItemCreateUpdateSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         return CartItem.objects.filter(cart__user=self.request.user)
 
-    def perform_create(self, serializer):
-        cart_item = serializer.save()
-        # Assuming CartItem has a 'cart' ForeignKey to Cart model
-        cart_item.cart.user = self.request.user
-        cart_item.cart.save()
-
-    def retrieve(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = CartItemSerializer(instance)
-        return Response(serializer.data)
-
-    def update(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = CartItemSerializer(instance, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-
-    def destroy(self, request, *args, **kwargs):
-        instance = self.get_object()
-        self.perform_destroy(instance)
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-    def perform_destroy(self, instance):
-        instance.delete()
+    def get_serializer_class(self):
+        if self.action in ["list", "retrieve"]:
+            return CartItemListRetrieveSerializer
+        return CartItemCreateUpdateSerializer
