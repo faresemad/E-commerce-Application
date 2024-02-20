f478bc68bedd2de7840920fbb4f8b6ad9ac3ee1b
Fares Emad <faresemadx@gmail.com>
2024-02-20 13:04:41 +0200

    feat: Add total_cost field to OrderSerializer

--- apps/orders/api/serializers.py
+++ apps/orders/api/serializers.py
@@ -16,7 +16,11 @@ class OrderItemSerializer(serializers.ModelSerializer):
 
 class OrderSerializer(serializers.ModelSerializer):
     items = OrderItemSerializer(many=True, read_only=True)
+    total_cost = serializers.SerializerMethodField()
 
     class Meta:
         model = Order
-        fields = ("id", "user", "items", "total", "created_at", "updated_at")
+        fields = ("id", "user", "items", "created_at", "updated_at", "total_cost")
+
+    def get_total_cost(self, obj: Order):
+        return obj.get_total_cost()
