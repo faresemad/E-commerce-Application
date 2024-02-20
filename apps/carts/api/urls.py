ad542f3496348244de414ac015d74ebd8d5cb828
Fares Emad <faresemadx@gmail.com>
2024-02-20 13:14:36 +0200

    fix: Add CartItemViewSet to API URLs and views
    This commit adds the `CartItemViewSet` to the API URLs and views. It also includes the necessary changes to the `CartViewSet` to handle cart items.

--- apps/carts/api/urls.py
+++ apps/carts/api/urls.py
@@ -1,7 +1,8 @@
 from rest_framework.routers import DefaultRouter
 
-from apps.carts.api.views import CartViewSet
+from apps.carts.api.views import CartItemViewSet, CartViewSet
 
 router = DefaultRouter()
 router.register(r"carts", CartViewSet, basename="carts")
+router.register(r"cart-items", CartItemViewSet, basename="cart-items")
 urlpatterns = router.urls
--- apps/carts/api/views.py
+++ apps/carts/api/views.py
@@ -2,8 +2,8 @@ from rest_framework import mixins, status, viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from apps.carts.api.serializers import CartSerializer
-from apps.carts.models import Cart
+from apps.carts.api.serializers import CartItemSerializer, CartSerializer
+from apps.carts.models import Cart, CartItem
 
 
 class CartViewSet(
@@ -20,7 +20,9 @@ class CartViewSet(
         return Cart.objects.filter(user=self.request.user)
 
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+        cart = serializer.save()
+        cart.user = self.request.user
+        cart.save()
 
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
@@ -42,3 +44,43 @@ class CartViewSet(
     def perform_destroy(self, instance):
         instance.products.clear()
         instance.save()
+
+
+class CartItemViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    viewsets.GenericViewSet,
+):
+    serializer_class = CartItemSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return CartItem.objects.filter(cart__user=self.request.user)
+
+    def perform_create(self, serializer):
+        cart_item = serializer.save()
+        # Assuming CartItem has a 'cart' ForeignKey to Cart model
+        cart_item.cart.user = self.request.user
+        cart_item.cart.save()
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = CartItemSerializer(instance)
+        return Response(serializer.data)
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = CartItemSerializer(instance, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def perform_destroy(self, instance):
+        instance.delete()
