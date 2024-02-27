eed55747fb00991e78923d545b2a8b6dd34efad8
Fares Emad <faresemadx@gmail.com>
2024-02-27 23:40:52 +0200

    fix: Change notification group name in test function

--- apps/notifications/views.py
+++ apps/notifications/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import HttpResponse
 def test(request):
     channel_layer = get_channel_layer()
     async_to_sync(channel_layer.group_send)(
-        "notification_test",
+        "notification_broadcast",
         {
             "type": "notification_message",
             "message": "This is a test message",
