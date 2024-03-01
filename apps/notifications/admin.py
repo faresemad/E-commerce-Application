62e57ffc5243962f3008fe4bcd3d0cdf2a77920b
Fares Emad <faresemadx@gmail.com>
2024-03-01 22:05:53 +0200

    feat: Add admin functionality for BroadcastNotification model

--- apps/notifications/admin.py
+++ apps/notifications/admin.py
@@ -1,1 +1,21 @@
-from django.contrib import admin  # noqa
+from django.contrib import admin
+
+from apps.notifications.models import BroadcastNotification
+
+
+@admin.register(BroadcastNotification)
+class BroadcastNotificationAdmin(admin.ModelAdmin):
+    list_display = ("message", "sent", "created_at")
+    list_filter = ("sent", "created_at")
+    search_fields = ("message",)
+    readonly_fields = ("created_at",)
+    fieldsets = (
+        (None, {"fields": ("message", "sent")}),
+        ("Date Information", {"fields": ("created_at",), "classes": ("collapse",)}),
+    )
+    ordering = ("-created_at",)
+    actions = ["mark_as_sent"]
+
+    @admin.action(description="Mark selected notifications as sent")
+    def mark_as_sent(self, request, queryset):
+        queryset.update(sent=True)
